7a615f2f09e78436f797580a461ccf95bc764285
김수민 <minsoom48@gmail.com>
2025-01-20 19:52:06 +0900

    deleted-api

--- auth-api/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signup, login, kakaoLogin } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/kakao', kakaoLogin);
-
-module.exports = router;
