2b3ae6bcab368b4e567a6ab9374ba61225d72a43
Jihyun <144994656+dxxrjh@users.noreply.github.com>
2025-02-16 21:49:17 +0900

    Fix: 반환 개수

--- src/repositories/besttaste.repository.js
+++ src/repositories/besttaste.repository.js
@@ -3,6 +3,6 @@ import { prisma } from "../../db.config.js";
 export const bestTasteList = async(req,res,next) => {
     return await prisma.cocktails.findMany({
         orderBy: { likes: "desc" },
-        take: 4,
+        take: 8,
       });
 }
