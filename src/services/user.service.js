973ba1880cfdf67a11c5f0774836e0d4b2eb3875
Hyekyung <88281367+gradeten@users.noreply.github.com>
2025-02-20 13:36:25 +0900

    feat: 카카오 로그인 추가

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const SALT_ROUNDS = 12;
 
-const generateToken = (userId) => {
+export const generateToken = (userId) => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET 환경 변수가 설정되지 않았습니다.");
   }
